4eb73499b77069af669b0fe2f314921ac446e132
chaarlie130 <chaarliecittadini@gmail.com>
2021-03-31 21:51:24 -0700

    Update fullscreen.plugin.js

--- fullscreen.plugin.js
+++ fullscreen.plugin.js
@@ -3,56 +3,33 @@
  * @displayName FullScreen
  * @authorId 748736662560833616
  * @website https://github.com/chaarlie130
- * @source https://raw.githubusercontent.com/CapnKitten/BetterDiscord/master/Plugins/HideChannels/HideChannels.plugin.js
- * @donate https://paypal.me/capnkitten
- * @invite jzJkA6Z
+ * @source https://raw.githubusercontent.com/chaarlie130/FullScreenDiscord/main/fullscreen.plugin.js
+ * @donate https://paypal.me/charlie230?locale.x=en_US
+ * @invite uJJeBSjR2g
  */
-
-/*@cc_on
-@if (@_jscript)
-	
-	// Offer to self-install for clueless users that try to run this directly.
-	var shell = WScript.CreateObject("WScript.Shell");
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
-	var pathSelf = WScript.ScriptFullName;
-	// Put the user at ease by addressing them in the first person
-	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-	} else if (!fs.FolderExists(pathPlugins)) {
-		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-		// Show the user where to put plugins in the future
-		shell.Exec("explorer " + pathPlugins);
-		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-	}
-	WScript.Quit();
-@else@*/
 
 module.exports = (() => {
 	const config = {
 		info: {
-			name: "HideChannels",
+			name: "FullScreen Discord",
 			authors: [
 				{
-					name: "CapnKitten",
-					discord_id: "405126960902176768",
-					github_username: "CapnKitten"
+					name: "Charlie",
+					discord_id: "748736662560833616",
+					github_username: "chaarlie130"
 				}
 			],
-			version: "1.0.6.1",
-			description: "Allows you to hide the channels list in servers and DMs.",
-			github: "https://github.com/CapnKitten/BetterDiscord/blob/master/Plugins/HideChannels/HideChannels.plugin.js",
-			github_raw: "https://raw.githubusercontent.com/CapnKitten/BetterDiscord/master/Plugins/HideChannels/HideChannels.plugin.js"
+			version: "1.0.0.0",
+			description: "Minimizes All discord Text channels, VC's and server tabs.",
+			github: "https://raw.githubusercontent.com/chaarlie130/FullScreenDiscord/main/fullscreen.plugin.js",
+			github_raw: "https://raw.githubusercontent.com/chaarlie130/FullScreenDiscord/main/fullscreen.plugin.js"
 		},
 		changelog: [
 			{
-				title: "Bug Fix",
+				title: "Initial Commit",
 				type: "added",
 				items: [
-					"Fixed a bug where the toggle button would stop working"
+					"Initial Release version 1.0.0.0"
 				]
 			}
 		]
